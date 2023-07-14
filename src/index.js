f9ced2d7c9b5062cdda66a4f26f22b1b55f12772
George <georgefbrockman@gmail.com>
2023-07-13 19:13:47 -0500

    adds functionality to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+function imageLoader(){
+    return fetch(imgUrl)
+    .then(res => res.json())
+    .then(images => addImages(images.message));
+}
+
+function addImages(array){
+    
+    array.forEach(e => {
+        
+        let image = document.createElement('img');
+        image.setAttribute('src', e);
+        document.querySelector('#dog-image-container').appendChild(image);
+    })
+}
+
+function breedLoader(){
+    
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(breeds => {
+        const array = Object.keys(breeds.message);
+        addBreeds(array)
+        filterer(array)
+    })
+}
+
+function addBreeds(array){
+
+    document.querySelector('#dog-breeds').innerHTML = ''
+
+    array.forEach(e => {
+        let p = document.createElement('p');
+        p.textContent = e;
+        document.querySelector('#dog-breeds').appendChild(p);
+    })
+}
+
+function filterer(array){
+    document.querySelector('#breed-dropdown').addEventListener('change', () => {
+        const dropDown = document.querySelector('#breed-dropdown');
+        const letter = dropDown.value;
+        const filtered = array.filter(breed => breed.startsWith(letter));
+        addBreeds(filtered);
+    })
+}
+
+function init(){
+    imageLoader();
+    breedLoader();
+}
+
+document.addEventListener('DOMContentLoaded', init)
